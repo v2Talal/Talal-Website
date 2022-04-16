a67af5d9dd2165e786fbe17dbf5c653a4c30de12
ALMJHOL2344 <ALMJHOL2344@users.noreply.github.com>
2022-04-16 13:56:01 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    new Typed('.firstText', {
+        strings: ["Hi and welcome,  I'm v2Talal 👋"],
+        typeSpeed: 25,
+        startDelay: 900,
+        showCursor: false
+      });
+    }),
+    new Typed('.personalInfo', {
+        strings: ["From Kuwait, I Love My Just Pc, I'm Joking Joking i love all 😅"],
+        typeSpeed: 25,
+        startDelay: 1900,
+        showCursor: false
+    }),
+    new Typed('.mediaInfoStart', {
+        strings: ["You can check out my - ضيفني لا تنسى بالبرامج الثانية "],
+        typeSpeed: 35,
+        startDelay: 5500,
+        showCursor: false
+    }),
+    new Typed('.mediaInfoEnd', {
+        strings: ["By clicking the icon down below"],
+        typeSpeed: 25,
+        startDelay: 8000,
+        showCursor: false
+    }),
+    new Typed('.mailInfo', {
+        strings: ["if you want contact at my email <a href='mailto:me@Talal.pro'>me@Talal.pro</a><span class='cursor'>_</span>"],
+        typeSpeed: 25,
+        startDelay: 8500,
+        showCursor: false
+    });
+
