6525b9d6ad8935097247325b69eafc60cb6149bf
v2Talal <102476581+v2Talal@users.noreply.github.com>
2022-05-04 22:13:14 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,27 +7,27 @@ $(document).ready(function () {
       });
     }),
     new Typed('.personalInfo', {
-        strings: ["From Kuwait, I Love My Just Pc, I'm Joking Joking i love all 😅"],
-        typeSpeed: 25,
-        startDelay: 1900,
+        strings: ["From Kuwait, I Love My Just Pc, I'm Joking Joking i love all 😅", " اهلا وسهلا ومرحباا", "ماتعرفني صح انا اعرفك انت اهو انت صح ؟", "From Kuwait, I Love My Just Pc, I'm Joking Joking i love all 😅"],
+        typeSpeed: 35,
+        startDelay: 2000,
         showCursor: false
     }),
     new Typed('.mediaInfoStart', {
         strings: ["You can check out my - ضيفني لا تنسى بالبرامج الثانية "],
-        typeSpeed: 35,
-        startDelay: 5500,
+        typeSpeed: 25,
+        startDelay: 5000,
         showCursor: false
     }),
     new Typed('.mediaInfoEnd', {
-        strings: ["By clicking the icon down below"],
-        typeSpeed: 25,
-        startDelay: 8000,
+        strings: ["By clicking the icon down below - اضغط الايقونه الي تحت وضيفني"],
+        typeSpeed: 23,
+        startDelay: 7000,
         showCursor: false
     }),
     new Typed('.mailInfo', {
-        strings: ["if you want contact at my email <a href='mailto:me@Talal.pro'>me@Talal.pro</a><span class='cursor'>_</span>"],
+        strings: ["if you want contact at my email:  <a href='mailto:me@Talal.pro'>me@Talal.pro</a><span class='cursor'>_</span>"],
         typeSpeed: 25,
-        startDelay: 8500,
+        startDelay: 9500,
         showCursor: false
     });
 
